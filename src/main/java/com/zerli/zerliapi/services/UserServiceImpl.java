d0be7903cc980ac30aeef90d9d32b5b82ca79049
ilyaveruk <ilyaveruk@gmail.com>
2022-09-14 11:07:28 +0300

    complete security for app , make changes to connect mysql database instead of fake users in commandline runner

--- src/main/java/com/zerli/zerliapi/services/UserServiceImpl.java
+++ src/main/java/com/zerli/zerliapi/services/UserServiceImpl.java
@@ -5,23 +5,32 @@ import com.zerli.zerliapi.entity.UserEntity;
 import com.zerli.zerliapi.model.User;
 import com.zerli.zerliapi.repository.RoleRepository;
 import com.zerli.zerliapi.repository.UserRepository;
+import javafx.print.Collation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl implements UserService, UserDetailsService {
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
 
 
     @Override
     public UserEntity saveUser(UserEntity userEntity) {
-
+        userEntity.setPassword(passwordEncoder.encode(userEntity.getPassword()));
         return userRepository.save(userEntity);
     }
 
@@ -45,6 +54,17 @@ public class UserServiceImpl implements UserService {
     @Override
     public List<UserEntity> getUsers() {
         return userRepository.findAll();
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found in the database");
+        }
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        user.getRoles().forEach(role -> authorities.add(new SimpleGrantedAuthority(role.getName())));
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), authorities);
     }
 }
 
